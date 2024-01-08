2c589669f068bba8f1ad3d4ce485ebcbb8f74320
sorutrt-wsl <kokishiono.58@gmail.com>
2024-01-08 22:23:54 +0900

    delete: bootcheckfile

--- src/bootcheck.ts
+++
@@ -1,9 +0,0 @@
-import { client } from "./main";
-
-// Botの起動確認
-client.once("ready", () => {
-    console.log("Ready!");
-    if (client.user) {
-        module.exports = client.user.tag;
-    }
-});
