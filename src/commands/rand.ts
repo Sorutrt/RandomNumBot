f4ac04116fa96607dfdbc8b4ddb1b5d9840a7d10
sorutrt-wsl <kokishiono.58@gmail.com>
2024-01-15 23:41:26 +0900

    ES6のimportに従ったexport

--- src/commands/rand.ts
+++ src/commands/rand.ts
@@ -4,6 +4,6 @@ export const data = new SlashCommandBuilder()
 	.setName('rand')
 	.setDescription('乱数を返します');
 
-export async function execute(interaction) {
+export async function execute(interaction: CommandInteraction) {
 	await interaction.reply('random number');
 }
