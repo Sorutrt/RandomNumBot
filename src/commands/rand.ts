d917b09ba19a2b61340975838572040b216ecb54
sorutrt-wsl <kokishiono.58@gmail.com>
2024-01-28 21:18:38 +0900

    add: randkosuオプションにmin,maxを追加

--- src/commands/rand.ts
+++ src/commands/rand.ts
@@ -6,25 +6,29 @@ export const data = new SlashCommandBuilder()
 	.addIntegerOption(option =>
 		option.setName('randkosu')
 			.setDescription('乱数の個数')
+			.setMinValue(1)
+			.setMaxValue(38)
 			.setRequired(true))
 
 export async function rand(interaction: any) {
 	try {
 		const randkosu = await interaction.options.getInteger('randkosu');
 		const randnumbers: number[] = new Array<number>();
-		for(let i=0; i<randkosu; i++) {
+		for(let i=0; i<randkosu; ) {
 			const randnum: number = Math.ceil(Math.random() * 38);
 			if (!randnumbers.includes(randnum)) { //値がrandnumbersに追加されていなければ真
 				randnumbers.push(randnum);
-			}
-			else {
-				i--;
+				i++;
 			}
 		}
 		
-		await interaction.reply(randnumbers);
+		randnumbers.sort((a, b) => a - b);
+
+		// output
+		await interaction.reply('Random Numbers: ' + randnumbers.toString());
 	}
 	catch (e) {
-		console.log(e);
+		console.log('rand.ts error')
+		console.error(e);
 	}
 }
